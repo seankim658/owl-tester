a1d6f171946bf7b349825740dc1b22fd503491ea
Sean Kim <33474168+seankim658@users.noreply.github.com>
2023-11-21 19:17:24 -0500

    update properties parsing

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,12 @@
 from owlready2 import *
+import csv
+
 
 ### owlready2 based functions 
 
 def or2_explore_classes(output_file: str, classes: list) -> None:
-    ''' Explore the ontology classes and writes the data to a text file.
+    ''' Explore the ontology classes and writes the data to an output file. Supports
+    txt and csv files. 
 
     Parameters
     ----------
@@ -13,22 +16,38 @@ def or2_explore_classes(output_file: str, classes: list) -> None:
         List of the classes to parse from owlready2.
     '''
 
-    with open(output_file, 'w') as f:
-        for cls in classes:
-            f.write(f'CLASS: {cls}\n')
-            f.write(f'\tNAME:           {cls.name}\n')
-            f.write(f'\tLABEL:          {cls.label}\n')
-            f.write(f'\tPARENT:         {cls.is_a}\n')
-            f.write(f'\tIRI:            {cls.iri}\n')
-            f.write(f'\tSTORID:         {cls.storid}\n')
-            f.write(f'\tNAMESPACE:      {cls.namespace}\n')
-            f.write(f'\tANNOTATIONS:\n')
-            for idx, annotation in enumerate(cls.comment):
-                f.write(f'\t\tAnnotation #{idx + 1}: {annotation}\n')
-            f.write(f'\n')
+    if output_file.endswith('.txt'):
+        output_type = 'txt'
+    elif output_file.endswith('.csv'):
+        output_type = 'csv'
+    else:
+        raise ValueError(f'Unsupported file type.')
+
+    if output_type == 'txt':
+        with open(output_file, 'w') as f:
+            for cls in classes:
+                f.write(f'CLASS: {cls}\n\n')
+                f.write(f'\tNAME:           {cls.name}\n')
+                f.write(f'\tLABEL:          {cls.label}\n')
+                f.write(f'\tPARENT:         {cls.is_a}\n')
+                f.write(f'\tIRI:            {cls.iri}\n')
+                f.write(f'\tSTORID:         {cls.storid}\n')
+                f.write(f'\tNAMESPACE:      {cls.namespace.base_iri}\n')
+                f.write(f'\tANNOTATIONS:\n')
+                for idx, annotation in enumerate(cls.comment):
+                    f.write(f'\t\tAnnotation #{idx + 1}: {annotation}\n')
+                f.write(f'\n')
+    elif output_type == 'csv':
+        with open(output_file, 'w', newline = '') as f:
+            writer = csv.writer(f)
+            writer.writerow(['class', 'name', 'label', 'parent', 'iri', 'storid', 'namespace', 'annotations'])
+            for cls in classes:
+                annotations = [f'Annotation #{idx + 1}: {annotation}' for idx, annotation in enumerate(cls.comment)]
+                writer.writerow([cls, cls.name, cls.label, cls.is_a, cls.iri, cls.storid, cls.namespace.base_iri] + annotations)
 
 def explore_properties(output_file: str, properties: list, property_type: str) -> None:
-    ''' Explore the data or object properties and write the data to a text file.
+    ''' Explore the data or object properties and write the data to a text file. Supports 
+    txt and csv files.
 
     Parameters
     ----------
@@ -37,13 +56,34 @@ def explore_properties(output_file: str, properties: list, property_type: str) -
     properties: list
         List of the properties to parse from owlready2.
     property_type: str
-        Whether the properties being parsed are object or data properties. 
+        Whether the properties being parsed are object, data, or annotation properties or all properties (accepts 'object', 'data', 'annotation', 'all').
     '''
 
-    with open(output_file, 'w') as f:
-        for property in properties:
-            f.write(f'{property_type.upper()} PROPERTY: {property}\n')
-            f.write(f'\tLABEL:      {property.label}\n')
-            f.write(f'\tIRI:        {property.iri}\n')
-            f.write(f'\t DOMAIN:    {property.domain}\n')
-            f.write(f'\tRANGE:      {property.range}')
+    if output_file.endswith('.txt'):
+        output_type = 'txt'
+    elif output_file.endswith('.csv'):
+        output_type = 'csv'
+    else:
+        raise ValueError(f'Unsupported file type.')
+
+    if output_type == 'txt':
+        with open(output_file, 'w') as f:
+            for property in properties:
+                if property_type == 'all':
+                    f.write(f'PROPERTY: {property}\n\n')
+                elif property_type == 'data' or property_type == 'object' or property_type == 'annotation':
+                    f.write(f'{property_type.upper()} PROPERTY: {property}\n\n')
+                f.write(f"\tLABEL:          {getattr(property, 'label', 'Not Specified')}\n")
+                f.write(f"\tIRI:            {getattr(property, 'iri', 'Not specified')}\n")
+                f.write(f"\tDOMAIN:         {getattr(property, 'domain', 'Not specified')}\n")
+                f.write(f"\tRANGE:          {getattr(property, 'range', 'Not specified')}\n")
+                f.write(f"\tCOMMENT:        {getattr(property, 'comment', 'Not specified')}\n\n")
+    elif output_type == 'csv':
+        with open(output_file, 'w', newline = '') as f:
+            writer = csv.writer(f)
+            if property_type == 'all':
+                writer.writerow([f'property', 'label', 'iri', 'domain', 'range', 'comment'])
+            elif property_type == 'data' or property_type == 'object' or property_type == 'annotation':
+                writer.writerow([f'{property_type}_property', 'label', 'iri', 'domain', 'range', 'comment'])
+            for property in properties:
+                writer.writerow([property, property.label, property.iri, property.domain, property.range, property.comment])
