425888b3a7ef3393673e4ffd0795a1e999edf297
Sean Kim <33474168+seankim658@users.noreply.github.com>
2023-11-22 12:49:13 -0500

    deprecated owlready2 functions

--- src/functions.py
+++ src/functions.py
@@ -1,8 +1,20 @@
-from owlready2 import *
+# from owlready2 import *
 import csv
 
+### ontospy functions ### 
+
+
+
+
+### owlready2 functions (DEPRECATED, if using these expect issues) ### 
+
 def _get_output_type(output_path: str) -> str:
-    '''
+    ''' Get the output file type from the output_path.
+
+    Parameters
+    ----------
+    output_path: str 
+        The user inputted output_path. 
     '''
     if output_path.endswith('.txt'):
         return 'txt'
@@ -29,8 +41,8 @@ def or2_explore_classes(output_file: str, classes: list) -> None:
 
     if output_type == 'txt':
         with open(output_file, 'w') as f:
-            for cls in classes:
-                f.write(f'CLASS: {cls}\n\n')
+            for idx, cls in enumerate(classes):
+                f.write(f'CLASS #{idx + 1}: {cls}\n\n')
                 f.write(f'\tNAME:           {cls.name}\n')
                 f.write(f'\tLABEL:          {cls.label}\n')
                 f.write(f'\tPARENT:         {cls.is_a}\n')
@@ -50,7 +62,7 @@ def or2_explore_classes(output_file: str, classes: list) -> None:
                 writer.writerow([cls, cls.name, cls.label, cls.is_a, cls.iri, cls.storid, cls.namespace.base_iri] + annotations)
 
 def explore_properties(output_file: str, properties: list, property_type: str) -> None:
-    ''' Explore the data or object properties and write the data to a text file. Supports 
+    ''' Explore the data or object properties and write the data to an output file. Supports 
     txt and csv files.
 
     Parameters
@@ -67,11 +79,11 @@ def explore_properties(output_file: str, properties: list, property_type: str) -
 
     if output_type == 'txt':
         with open(output_file, 'w') as f:
-            for property in properties:
+            for idx, property in enumerate(properties):
                 if property_type == 'all':
-                    f.write(f'PROPERTY: {property}\n\n')
+                    f.write(f'PROPERTY #{idx + 1}: {property}\n\n')
                 elif property_type == 'data' or property_type == 'object' or property_type == 'annotation':
-                    f.write(f'{property_type.upper()} PROPERTY: {property}\n\n')
+                    f.write(f'{property_type.upper()} PROPERTY #{idx + 1}: {property}\n\n')
                 f.write(f"\tLABEL:          {getattr(property, 'label', 'Not Specified')}\n")
                 f.write(f"\tIRI:            {getattr(property, 'iri', 'Not specified')}\n")
                 f.write(f"\tDOMAIN:         {getattr(property, 'domain', 'Not specified')}\n")
@@ -88,14 +100,22 @@ def explore_properties(output_file: str, properties: list, property_type: str) -
                 writer.writerow([property, property.label, property.iri, property.domain, property.range, property.comment])
 
 def explore_individuals(output_file: str, individuals: list) -> None:
-    '''
+    ''' Explore the ontology defined individuals and write the data to an output file. Supports 
+    txt and csv files.
+
+    Parameters
+    ----------
+    output_file: str
+        Where to send the output information.
+    individuals: list
+        List of the individuals to parse from owlready2. 
     '''
 
     output_type = _get_output_type(output_file)
 
     if output_type == 'txt':
         with open(output_file, 'w') as f:
-            for individual in individuals:
+            for idx, individual in enumerate(individuals):
                 f.write(f'INDIVIDUAL: {individual}\n\n')
                 f.write(f'\tNAME      {individual.name}\n')
                 f.write(f'\tLABEL:    {individual.label}\n')
